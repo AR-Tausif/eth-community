68f766b0804c73e49978bb5fd7c7f566f16cd96f
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2024-03-11 19:11:13 +0000

    try to fix feedback section design breaking

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,16 +132,14 @@
       infinite: true,
       dots: true,
       arrows: true,
-      centerPadding: "15px",
-      centerMode: true,
       slidesToShow: 3,
-      slidesToScroll: 1,
+      slidesToScroll: 3,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
             infinite: true,
             dots: true,
             arrows: true,
@@ -161,6 +159,7 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: false,
+            dots:false,
           },
         },
       ],
